e3fa66bca7940935a0c7628f17e835231912fdcd
Tama Sarker <33072138+tamasarker1434@users.noreply.github.com>
2025-06-13 23:07:51 +0200

    Code Cleaning

--- sink-service/src/main/java/com/dapm2/sink/repository/MiningResultRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.dapm2.sink.repository;
-//
-//import com.dapm2.sink.entity.MiningResult;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import org.springframework.stereotype.Repository;
-//
-//@Repository
-//public interface MiningResultRepository extends JpaRepository<MiningResult, Long> {
-//}
