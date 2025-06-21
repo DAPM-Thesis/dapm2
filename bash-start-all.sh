0955a3057de4e033e504206d5ce589a337691f50
Tama Sarker <33072138+tamasarker1434@users.noreply.github.com>
2025-06-21 12:24:07 +0200

    update package and script ps and bash

--- bash-start-all.sh
+++ bash-start-all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Bash script to start all Docker Compose stacks in parallel
 
+(docker compose up --build -d) &
 (cd ingestion-service && docker compose up --build -d) &
 (cd operator-service && docker compose up --build -d) &
 (cd sink-service && docker compose up --build -d) &
