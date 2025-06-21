510e953767222c7b90133b99261bc40a1f34e095
Tama Sarker <33072138+tamasarker1434@users.noreply.github.com>
2025-06-21 12:23:16 +0200

    update package and script

--- start-all.ps1
+++ start-all.ps1
@@ -1,3 +1,4 @@
+Start-Process powershell -ArgumentList "docker compose up --build -d"
 Start-Process powershell -ArgumentList "cd .\ingestion-service; docker compose up --build -d"
 Start-Process powershell -ArgumentList "cd .\operator-service; docker compose up --build -d"
 Start-Process powershell -ArgumentList "cd .\sink-service; docker compose up --build -d"
